8d8d1384717b8db74943eb97ecf59ed098c86f28
matthew chimenti <matthewosu@gmail.com>
2021-10-25 15:10:53 -0700

    first commit

---
+++ myapp/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from .views import BaseView, AboutView, FooterView, MembersView, SupportView, EventsView, ContactView, StayUpToDateView
+
+ 
+app_name = 'myapp'
+urlpatterns = [
+    path('', BaseView, name='base'),
+    path('footer/', FooterView, name="footer"),
+    path('about/', AboutView, name='about'),
+    path('members/', MembersView, name="members"),
+    path('support/', SupportView, name="support"),
+    path('events/', EventsView, name="events"),
+    path('contact/', ContactView, name="contact"),
+    path('stayuptodate/', StayUpToDateView, name="stayuptodate"),
+
+    # path('myview/', views.myview, name='myview'),
+    # path('mycreate/', views.mycreate, name='mycreate')
+]
---
+++ myapp/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, reverse
+from django.http import HttpResponseRedirect, HttpResponse, request
+from .models import MyModel
+
+
+def BaseView(request):
+    return render(request, 'myapp/landing.html')
+
+def FooterView(request):
+    return render(request, 'myapp/footer.html')
+
+def AboutView(request):
+    return render(request, 'myapp/about.html')
+
+def MembersView(request):
+    return render(request, 'myapp/members.html')
+
+def SupportView(request):
+    return render(request, 'myapp/support.html')
+    
+def EventsView(request):
+    return render(request, 'myapp/events.html')
+
+def ContactView(request):
+    return render(request, 'myapp/contact.html')
+
+def StayUpToDateView(request):
+    return render(request, 'myapp/stayuptodate.html')
+
+# heroku test push
+
+# def myview(request):
+#     myinstances = MyModel.objects.all()
+#     context = {
+#         'myinstances': myinstances
+#     }
+#     return render(request, 'myapp/pageb.html', context)
+
+# def mycreate(request):
+#     myfield = request.POST['myfield']
+#     mymodel = MyModel(myfield=myfield)
+#     mymodel.save()
+#     return HttpResponseRedirect(reverse('myapp:myview'))
