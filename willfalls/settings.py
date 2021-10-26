7cd157ff74e55bb9a7572aa939801c0648d6d4b8
matthew chimenti <matthewosu@gmail.com>
2021-10-26 12:28:21 -0700

    added allowed hosts for heroku

--- willfalls/settings.py
+++ willfalls/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ak*-m13fo6pu5(((xqu*67kt3x1z^wa#f)nh8ukw8d$q1q*6p5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
