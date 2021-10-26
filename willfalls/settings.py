0f379787e6950f6bec0fc458df41e89a9aad43e8
matthew chimenti <matthewosu@gmail.com>
2021-10-26 13:57:45 -0700

    added whitenoise to requirments.txt

--- willfalls/settings.py
+++ willfalls/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = ['willfallsymph.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
-    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
